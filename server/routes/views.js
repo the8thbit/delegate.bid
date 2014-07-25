6b4468e7d955be3d070cc495face97450e27e42e
Peter Sussman <the8thbit@gmail.com>
2014-07-25 02:28:04 -0500

    turned on jade error prettyprinting

--- server/routes/views.js
+++ server/routes/views.js
@@ -3,6 +3,7 @@ module.exports = function(app){
 	app.set('views', __dirname + '/../../client');
 	app.set('view engine', 'jade');
 	app.engine('jade', require('jade').__express);
+	app.configure('development', function(){ app.locals.pretty = true; });
 
 	//get the JADE template pages used in the project
 	app.get('/', function(req, res){ res.render('main/main'); });
