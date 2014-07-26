734a4a432107edaeafb114b4b6e92e4295424f45
Peter Sussman <the8thbit@gmail.com>
2014-07-25 23:09:02 -0500

    switched to an even better way to create controllers

--- client/main/main.js
+++ client/main/main.js
@@ -1,6 +1,7 @@
 var DELBID = angular.module('DELBID', ['ngRoute']);
 
-DELBID.controller('DelegateTableCtrl', function($scope) {
+DELBID.ctrls = {}
+DELBID.ctrls.mainCtrl = function($scope) {
 	$scope.delegates = [
 		{ name: 'btsx.chinesecommunity', owner: 'ripplexiaoshan', category: 'faucet',      desc: 'The faucet is for new users from China',                     pay: 50, ownerpay: 50,  uptime: 99.5, version: '0.2.3' },
 		{ name: 'bdnoble',               owner: 'bdnoble1206',    category: 'reliability', desc: 'Please vote for bdnoble: High Reliability.',                 pay: 99, ownerpay: 100, uptime: 99,   version: '0.2.3' },
@@ -8,4 +9,5 @@ DELBID.controller('DelegateTableCtrl', function($scope) {
 		{ name: 'commonwealth',          owner: '8bit',           category: 'enhancement', desc: 'Giving back to the bitshares community with a 0% pay rate.', pay: 0,  ownerpay: 0,   uptime: 90,   version: '0.2.3' },
 		{ name: 'spartako',              owner: 'spartako',       category: 'reliability', desc: 'low pay rate, high reliability.',                            pay: 50, ownerpay: 100, uptime: 99,   version: '0.2.3' }
 	];
-});
+}
+DELBID.controller(DELBID.ctrls);
