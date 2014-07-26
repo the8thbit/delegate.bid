3dad7e3e4ab6cedd7f34dd2d4d30cbccdf2c8cc8
Peter Sussman <the8thbit@gmail.com>
2014-07-25 23:24:22 -0500

    switched back to old way to manage controllers

--- client/main/main.js
+++ client/main/main.js
@@ -1,7 +1,6 @@
-var DELBID = angular.module('DELBID', ['ngRoute']);
+var DELBID = angular.module('DELBID', []);
 
-DELBID.ctrls = {}
-DELBID.ctrls.mainCtrl = function($scope) {
+DELBID.controller('mainCtrl', function($scope) {
 	$scope.delegates = [
 		{ name: 'btsx.chinesecommunity', owner: 'ripplexiaoshan', category: 'faucet',      desc: 'The faucet is for new users from China',                     pay: 50, ownerpay: 50,  uptime: 99.5, version: '0.2.3' },
 		{ name: 'bdnoble',               owner: 'bdnoble1206',    category: 'reliability', desc: 'Please vote for bdnoble: High Reliability.',                 pay: 99, ownerpay: 100, uptime: 99,   version: '0.2.3' },
@@ -9,5 +8,4 @@ DELBID.ctrls.mainCtrl = function($scope) {
 		{ name: 'commonwealth',          owner: '8bit',           category: 'enhancement', desc: 'Giving back to the bitshares community with a 0% pay rate.', pay: 0,  ownerpay: 0,   uptime: 90,   version: '0.2.3' },
 		{ name: 'spartako',              owner: 'spartako',       category: 'reliability', desc: 'low pay rate, high reliability.',                            pay: 50, ownerpay: 100, uptime: 99,   version: '0.2.3' }
 	];
-}
-DELBID.controller(DELBID.ctrls);
+});
